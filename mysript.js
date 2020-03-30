0d7ccb9975e02a97a325cfb5803e4f06a7ccfbb6
2akash111998 <35839624+2akash111998@users.noreply.github.com>
2020-03-31 01:40:55 +0530

    JavaScript

---
+++ mysript.js
@@ -0,0 +1,94 @@
+var cells = document.getElementsByTagName('td')
+// HTMLCollection(9) [td, td, td, td, td, td, td, td, td]
+
+
+// On Click
+cells[0].addEventListener("click",function(){
+  cells[0].textContent = "X";
+})
+
+// On Double Click
+cells[0].addEventListener("dblclick",function(){
+  cells[0].textContent = "O";
+})
+
+// On Click
+cells[1].addEventListener("click",function(){
+  cells[1].textContent = "X";
+})
+
+// On Double Click
+cells[1].addEventListener("dblclick",function(){
+  cells[1].textContent = "O";
+})
+
+// On Click
+cells[2].addEventListener("click",function(){
+  cells[2].textContent = "X";
+})
+
+// On Double Click
+cells[2].addEventListener("dblclick",function(){
+  cells[2].textContent = "O";
+})
+
+// On Click
+cells[3].addEventListener("click",function(){
+  cells[3].textContent = "X";
+})
+
+// On Double Click
+cells[3].addEventListener("dblclick",function(){
+  cells[3].textContent = "O";
+})
+
+// On Click
+cells[4].addEventListener("click",function(){
+  cells[4].textContent = "X";
+})
+
+// On Double Click
+cells[4].addEventListener("dblclick",function(){
+  cells[4].textContent = "O";
+})
+
+// On Click
+cells[5].addEventListener("click",function(){
+  cells[5].textContent = "X";
+})
+
+// On Double Click
+cells[5].addEventListener("dblclick",function(){
+  cells[5].textContent = "O";
+})
+
+// On Click
+cells[6].addEventListener("click",function(){
+  cells[6].textContent = "X";
+})
+
+// On Double Click
+cells[6].addEventListener("dblclick",function(){
+  cells[6].textContent = "O";
+})
+
+// On Click
+cells[7].addEventListener("click",function(){
+  cells[7].textContent = "X";
+})
+
+// On Double Click
+cells[7].addEventListener("dblclick",function(){
+  cells[7].textContent = "O";
+})
+
+// On Click
+cells[8].addEventListener("click",function(){
+  cells[8].textContent = "X";
+})
+
+// On Double Click
+cells[8].addEventListener("dblclick",function(){
+  cells[8].textContent = "O";
+})
+
