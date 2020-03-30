368e15a80fd0e7691920314e55550b6dad7791af
Akash Sharma <35839624+2akash111998@users.noreply.github.com>
2020-03-31 01:59:29 +0530

    Improved: clears board upon reset

--- mysript.js
+++ mysript.js
@@ -1,6 +1,16 @@
+var restart = document.querySelector('#b')
+//<a id="b" class="btn btn-primary btn-lg" href="#" role="button">Restart!</a>
+
 var cells = document.getElementsByTagName('td')
 // HTMLCollection(9) [td, td, td, td, td, td, td, td, td]
 
+function clearBoard(){
+	for(var i=0;i<cells.length;i++){
+		cells[i].textContent = '';
+	}
+}
+
+restart.addEventListener('click', clearBoard)
 
 // On Click
 cells[0].addEventListener("click",function(){
